fb59dbf672b1a19ca3635616fb42d0b582e45072
Jeffrey Adorkor <adorkorj@gmail.com>
2021-05-01 19:16:46 +0000

    Fixed MEDIA ROOT

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     caption = models.CharField(max_length=255, blank=True)
     recipe = models.TextField(blank=False)
-    image = models.ImageField(upload_to='posts/', null=True)
+    image = models.ImageField(upload_to='post_pics', null=True)
 
     def __str__(self):
         return self.caption
