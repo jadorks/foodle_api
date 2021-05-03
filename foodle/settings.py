96b24100b7cf46ac449d47ed23e604a963fafc4d
Jeffrey Adorkor <adorkorj@gmail.com>
2021-05-03 01:16:17 +0000

    Added django_storages

--- foodle/settings.py
+++ foodle/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -188,5 +189,7 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 CORS_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
 
 
